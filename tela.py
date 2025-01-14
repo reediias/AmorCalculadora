68e656297f0cba479ea7987a883838210969af08
Renata Dias <r.dias@aluno.uepb.edu.br>
2025-01-14 14:23:22 -0300

    algumas alterações

--- tela.py
+++ tela.py
@@ -28,18 +28,36 @@ frameMeio.grid(row=1, column=0)
 tela = Label(frameCima, text="Calculadora do Amor", width=0, padx=3, anchor=NW, font=('Arial 20 bold'), bg=cor2, fg=cor6)
 tela.place(x=55, y=0)
 
+def escolher():
+    global telaImg, botaoImage
+
+    escolha1 = selecionado1.get()
+    escolha2 = selecionado2.get()
+
+    if escolha1 == 'Homem' and escolha2 == 'Homem':
+        imagem1 = 'casal2.png'
+
+        telaImg = Image.open(imagem1)
+        telaImg = telaImg.resize((120, 120))
+        telaImg = ImageTk.PhotoImage(telaImg)
+        telaLogo.config(image=telaImg)
+        telaLogo.image = telaImg 
+
+    elif escolha1 == 'Homem' and escolha2 == 'Mulher':
+        imagem2 = 'casal1.png'
+
+    elif escolha1 == 'Mulher' and escolha2 == 'Mulher':
+        imagem3 = 'casal3.png'
+
+    else:
+        imagem4 = 'coracao1.png'
+        
 telaImg = Image.open('coracao1.png')
 telaImg = telaImg.resize((120, 120))
 telaImg = ImageTk.PhotoImage(telaImg)
 
 telaLogo = Label(frameCima, image = telaImg, width=150, height=140, bg=cor2)
 telaLogo.place(x=10, y=50)
-
-#resultado = Label(frameCima, text="renata", width=35, padx=10, anchor=NW, font=('Arial 12 bold'), bg=cor1, fg=cor6)
-#resultado.place(x=170, y=70)
-
-#resultado1 = Label(frameCima, text="augusto", width=17, padx=10, anchor=CENTER, font=('Arial 12 bold'), bg=cor1, fg=cor6)
-#resultado1.place(x=170, y=100)
 
 resultado2 = Label(frameCima, text="50%", width=3, padx=10, anchor=CENTER, font=('Arial 25 bold'), bg=cor2, fg=cor6)
 resultado2.place(x=250, y=100)
@@ -58,15 +76,15 @@ enterNome2.place(x=210, y=40)
 
 selecionado1 = StringVar()
 
-rad1 = Radiobutton(frameMeio, text='Homem', bg=cor2, font=('Arial 10'), value='Homem', variable=selecionado1).place(x=20, y=80)
+rad1 = Radiobutton(frameMeio, command=escolher, text='Homem', bg=cor2, font=('Arial 10'), value='Homem', variable=selecionado1).place(x=20, y=80)
 
-rad2 = Radiobutton(frameMeio, text='Mulher', bg=cor2, font=('Arial 10'), value='mulher', variable=selecionado1).place(x=20, y=100)
+rad2 = Radiobutton(frameMeio, command=escolher, text='Mulher', bg=cor2, font=('Arial 10'), value='mulher', variable=selecionado1).place(x=20, y=100)
 
 selecionado2 = StringVar()
 
-rad1 = Radiobutton(frameMeio, text='Homem', bg=cor2, font=('Arial 10'), value='Homem', variable=selecionado2).place(x=206, y=80)
+rad1 = Radiobutton(frameMeio, command=escolher, text='Homem', bg=cor2, font=('Arial 10'), value='Homem', variable=selecionado2).place(x=206, y=80)
 
-rad2 = Radiobutton(frameMeio, text='Mulher', bg=cor2, font=('Arial 10'), value='mulher', variable=selecionado2).place(x=206, y=100)
+rad2 = Radiobutton(frameMeio, command=escolher, text='Mulher', bg=cor2, font=('Arial 10'), value='mulher', variable=selecionado2).place(x=206, y=100)
 
 linha1 = Label(frameMeio, width=0, height=1, anchor=NW, font=('Arial 10'), bg=cor4)
 linha1.place(x=195, y=42)
