e874f84318c38597d2ced46105589d9e359d1594
Renata Dias <r.dias@aluno.uepb.edu.br>
2025-01-14 21:12:49 -0300

    talvez o final

--- tela.py
+++ tela.py
@@ -68,13 +68,10 @@ def escolher():
         telaLogo.config(image=telaImg)
         telaLogo.image = telaImg
 
-cor1 = '#FF6347' #vermelho
+cor1 = '#D32F2F'#vermelho
 cor2 = '#FFDEAD' #pessego
-cor3 = '#3CB371' #verde
-cor4 = '#4682B4' #azul
-cor5 = '#FFFFFF' #branco
-cor6 = '#000000' #preto
-cor7 = '#8A2BE2' #roxo
+cor3 = '#FFFFFF' #branco
+cor4 = '#000000' #preto
 
 janela = Tk()
 janela.title("")
@@ -85,60 +82,60 @@ janela.resizable(width=False, height=False)
 estilo = ttk.Style(janela)
 estilo.theme_use('clam')
 
-frameCima = Frame(janela, width=418, height=200, bg=cor2)
+frameCima = Frame(janela, width=418, height=200, bg=cor1)
 frameCima.grid(row=0, column=0)
 
-frameMeio = Frame(janela, width=418, height=200, bg=cor2)
+frameMeio = Frame(janela, width=418, height=200, bg=cor1)
 frameMeio.grid(row=1, column=0)
 
-tela = Label(frameCima, text="Calculadora do Amor", width=0, padx=3, anchor=NW, font=('Arial 20 bold'), bg=cor2, fg=cor6)
-tela.place(x=55, y=0)
+tela = Label(frameCima, text="Calculadora do Amor", width=0, padx=3, anchor=NW, font=('Arial 20 bold'), bg=cor1, fg=cor4)
+tela.place(x=55, y=10)
 
 telaImg = Image.open('coracao1.png')
 telaImg = telaImg.resize((120, 120))
 telaImg = ImageTk.PhotoImage(telaImg)
 
-telaLogo = Label(frameCima, image=telaImg, width=150, height=140, bg=cor2)
+telaLogo = Label(frameCima, image=telaImg, width=150, height=140, bg=cor1)
 telaLogo.place(x=10, y=50)
 
-resultado = Label(frameCima, text="50%", width=3, padx=10, anchor=CENTER, font=('Arial 25 bold'), bg=cor2, fg=cor6)
+resultado = Label(frameCima, text="50%", width=3, padx=10, anchor=CENTER, font=('Arial 25 bold'), bg=cor1, fg=cor4)
 resultado.place(x=250, y=100)
 
-nome1 = Label(frameMeio, text="Seu nome", anchor=NW, font=('Arial 10 bold'), bg=cor2, fg=cor6)
-nome1.place(x=75, y=10)
+nome1 = Label(frameMeio, text="Seu nome", anchor=NW, font=('Arial 12 bold'), bg=cor1, fg=cor4)
+nome1.place(x=70, y=10)
 
 enterNome1 = Entry(frameMeio, width=15, font=('Arial 14'), justify='center', relief='solid')
 enterNome1.place(x=25, y=40)
 
-nome2 = Label(frameMeio, text="Nome do parceiro(a)", anchor=NW, font=('Arial 10 bold'), bg=cor2, fg=cor6)
-nome2.place(x=225, y=10)
+nome2 = Label(frameMeio, text="Nome do parceiro(a)", anchor=NW, font=('Arial 12 bold'), bg=cor1, fg=cor4)
+nome2.place(x=216, y=10)
 
 enterNome2 = Entry(frameMeio, width=15, font=('Arial 14'), justify='center', relief='solid')
 enterNome2.place(x=210, y=40)
 
 selecionado1 = StringVar()
 
-rad1 = Radiobutton(frameMeio, command=escolher, text='Homem', bg=cor2, font=('Arial 10'), value='Homem', variable=selecionado1).place(x=20, y=80)
+rad1 = Radiobutton(frameMeio, command=escolher, text='Homem', bg=cor1, font=('Arial 12 bold'), value='Homem', variable=selecionado1).place(x=20, y=80)
 
-rad2 = Radiobutton(frameMeio, command=escolher, text='Mulher', bg=cor2, font=('Arial 10'), value='Mulher', variable=selecionado1).place(x=20, y=100)
+rad2 = Radiobutton(frameMeio, command=escolher, text='Mulher', bg=cor1, font=('Arial 12 bold'), value='Mulher', variable=selecionado1).place(x=20, y=100)
 
 selecionado2 = StringVar()
 
-rad3 = Radiobutton(frameMeio, command=escolher, text='Homem', bg=cor2, font=('Arial 10'), value='Homem', variable=selecionado2).place(x=206, y=80)
+rad3 = Radiobutton(frameMeio, command=escolher, text='Homem', bg=cor1, font=('Arial 12 bold'), value='Homem', variable=selecionado2).place(x=206, y=80)
 
-rad4 = Radiobutton(frameMeio, command=escolher, text='Mulher', bg=cor2, font=('Arial 10'), value='Mulher', variable=selecionado2).place(x=206, y=100)
+rad4 = Radiobutton(frameMeio, command=escolher, text='Mulher', bg=cor1, font=('Arial 12 bold'), value='Mulher', variable=selecionado2).place(x=206, y=100)
 
-linha1 = Label(frameMeio, width=0, height=1, anchor=NW, font=('Arial 10'), bg=cor4)
+linha1 = Label(frameMeio, width=0, height=1, anchor=NW, font=('Arial 10'), bg=cor1)
 linha1.place(x=195, y=42)
 
-linha2 = Label(frameMeio, width=0, height=1, anchor=NW, font=('Arial 10'), bg=cor4)
+linha2 = Label(frameMeio, width=0, height=1, anchor=NW, font=('Arial 10'), bg=cor1)
 linha2.place(x=203, y=42)
 
-botaoImage = Image.open('coracao2.png')
+botaoImage = Image.open('coracao1.png')
 botaoImage = botaoImage.resize((30, 30))
 botaoImage = ImageTk.PhotoImage(botaoImage)
 
-calcularBotao = Button(frameMeio, command=calculo, image=botaoImage, width=150, text='Calcular \n Porcentagem'.upper(), height=24, compound=RIGHT, anchor=CENTER, font=('Arial 10 bold'), bg=cor4)
+calcularBotao = Button(frameMeio, command=calculo, image=botaoImage, width=150, text='Calcular \n Porcentagem'.upper(), height=24, compound=RIGHT, anchor=CENTER, font=('Arial 10 bold'), bg=cor3)
 calcularBotao.place(x=120, y=145)
 
 janela.mainloop()
